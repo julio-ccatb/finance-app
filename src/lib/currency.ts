abb181447c2c5bd479ef718031d93c1b25c1f257
Julio Castaño <julio_ccatb@hotmail.com>
2024-11-27 07:37:11 -0400

    CFormatCurrency minor change

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -2,5 +2,8 @@ export const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat("es-ES", {
     style: "currency",
     currency: "DOP",
+    currencyDisplay: "narrowSymbol",
+    // style: "unit",
+    // unit: "pound",
   }).format(amount);
 };
